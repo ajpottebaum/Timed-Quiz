970794822ed2bc4ee0f10e99c522c597a51c903d
anthony pottebaum <ajpottebaum@gmail.com>
2022-04-03 21:17:49 -0600

    javascript update

--- script.js
+++ script.js
@@ -1,147 +1,194 @@
-:root{
-    background-color: rgb(224, 233, 253);
+// Elements
+var quizBody = document.getElementById("quiz");
+var resultsEl = document.getElementById("result");
+var finalScoreEl = document.getElementById("finalScore");
+var gameoverDiv = document.getElementById("gameover");
+var questionsEl = document.getElementById("questions");
+var quizTimer = document.getElementById("timer");
+var startQuizButton = document.getElementById("startbtn");
+var startQuizDiv = document.getElementById("startpage");
+var highscoreContainer = document.getElementById("highscoreContainer");
+var highscoreDiv = document.getElementById("high-scorePage");
+var highscoreInputName = document.getElementById("initials");
+var highscoreDisplayName = document.getElementById("highscore-initials");
+var endGameBtns = document.getElementById("endGameBtns");
+var submitScoreBtn = document.getElementById("submitScore");
+var highscoreDisplayScore = document.getElementById("highscore-score");
+var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var buttonD = document.getElementById("d");
+
+// Quiz questions
+var quizQuestions = [{
+    question: "Which answer is letter A?",
+    choiceA: "A",
+    choiceB: "B",
+    choiceC: "C",
+    choiceD: "D",
+    correctAnswer: "a"},
+  {
+    question: "Which answer is letter C?",
+    choiceA: "A",
+    choiceB: "B",
+    choiceC: "C",
+    choiceD: "D",
+    correctAnswer: "c"},
+   {
+    question: "What is 1+1?",
+    choiceA: "1",
+    choiceB: "2",
+    choiceC: "3",
+    choiceD: "4",
+    correctAnswer: "b"},
+    {
+    question: "What is 1+3?",
+    choiceA: "1",
+    choiceB: "2",
+    choiceC: "3",
+    choiceD: "4",
+    correctAnswer: "d"}, 
+    ];
+// Other global variables
+var finalQuestionIndex = quizQuestions.length;
+var currentQuestionIndex = 0;
+var timeLeft = 76;
+var timerInterval;
+var score = 0;
+var correct;
+
+// This function cycles through the object array containing the quiz questions to generate the questions and answers.
+function generateQuizQuestion(){
+    gameoverDiv.style.display = "none";
+    if (currentQuestionIndex === finalQuestionIndex){
+        return showScore();
+    } 
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+    questionsEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
+    buttonA.innerHTML = currentQuestion.choiceA;
+    buttonB.innerHTML = currentQuestion.choiceB;
+    buttonC.innerHTML = currentQuestion.choiceC;
+    buttonD.innerHTML = currentQuestion.choiceD;
+};
+
+// Start Quiz function starts the TimeRanges, hides the start button, and displays the first quiz question.
+function startQuiz(){
+    gameoverDiv.style.display = "none";
+    startQuizDiv.style.display = "none";
+    generateQuizQuestion();
+
+    //Timer
+    timerInterval = setInterval(function() {
+        timeLeft--;
+        quizTimer.textContent = "Time left: " + timeLeft;
+    
+        if(timeLeft === 0) {
+          clearInterval(timerInterval);
+          showScore();
+        }
+      }, 1000);
+    quizBody.style.display = "block";
+}
+// This function is the end page screen that displays your score after either completeing the quiz or upon timer run out
+function showScore(){
+    quizBody.style.display = "none"
+    gameoverDiv.style.display = "flex";
+    clearInterval(timerInterval);
+    highscoreInputName.value = "";
+    finalScoreEl.innerHTML = "You got " + score + " out of " + quizQuestions.length + " correct!";
+}
+
+// On click of the submit button, we run the function highscore that saves and stringifies the array of high scores already saved in local stoage
+// as well as pushing the new user name and score into the array we are saving in local storage. Then it runs the function to show high scores.
+submitScoreBtn.addEventListener("click", function highscore(){
+    
+    
+    if(highscoreInputName.value === "") {
+        alert("Initials cannot be blank");
+        return false;
+    }else{
+        var savedHighscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+        var currentUser = highscoreInputName.value.trim();
+        var currentHighscore = {
+            name : currentUser,
+            score : score
+        };
+    
+        gameoverDiv.style.display = "none";
+        highscoreContainer.style.display = "flex";
+        highscoreDiv.style.display = "block";
+        endGameBtns.style.display = "flex";
+        
+        savedHighscores.push(currentHighscore);
+        localStorage.setItem("savedHighscores", JSON.stringify(savedHighscores));
+        generateHighscores();
+
     }
     
-    li{
-        padding-bottom: 3px;
-        color: white;
-        text-shadow: 1.5px 1px 3px black;
-        list-style-type: none;
+});
+
+// This function clears the list for the high scores and generates a new high score list from local storage
+function generateHighscores(){
+    highscoreDisplayName.innerHTML = "";
+    highscoreDisplayScore.innerHTML = "";
+    var highscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+    for (i=0; i<highscores.length; i++){
+        var newNameSpan = document.createElement("li");
+        var newScoreSpan = document.createElement("li");
+        newNameSpan.textContent = highscores[i].name;
+        newScoreSpan.textContent = highscores[i].score;
+        highscoreDisplayName.appendChild(newNameSpan);
+        highscoreDisplayScore.appendChild(newScoreSpan);
     }
-    
-    p{
-        color: black;
-        text-shadow: .5px .5px white;
-        text-align: center;
-        font-size: xx-large;
-        font-family: sans-serif;
-    
+}
+
+// This function displays the high scores page while hiding all of the other pages from 
+function showHighscore(){
+    startQuizDiv.style.display = "none"
+    gameoverDiv.style.display = "none";
+    highscoreContainer.style.display = "flex";
+    highscoreDiv.style.display = "block";
+    endGameBtns.style.display = "flex";
+
+    generateHighscores();
+}
+
+// This function clears the local storage of the high scores as well as clearing the text from the high score board
+function clearScore(){
+    window.localStorage.clear();
+    highscoreDisplayName.textContent = "";
+    highscoreDisplayScore.textContent = "";
+}
+
+// This function sets all the variables back to their original values and shows the home page to enable replay of the quiz
+function replayQuiz(){
+    highscoreContainer.style.display = "none";
+    gameoverDiv.style.display = "none";
+    startQuizDiv.style.display = "flex";
+    timeLeft = 76;
+    score = 0;
+    currentQuestionIndex = 0;
+}
+
+// This function checks the response to each answer 
+function checkAnswer(answer){
+    correct = quizQuestions[currentQuestionIndex].correctAnswer;
+
+    if (answer === correct && currentQuestionIndex !== finalQuestionIndex){
+        score++;
+        alert("That Is Correct!");
+        currentQuestionIndex++;
+        generateQuizQuestion();
+        //display in the results div that the answer is correct.
+    }else if (answer !== correct && currentQuestionIndex !== finalQuestionIndex){
+        alert("That Is Incorrect.")
+        currentQuestionIndex++;
+        generateQuizQuestion();
+        //display in the results div that the answer is wrong.
+    }else{
+        showScore();
     }
-    h2{
-        font-family: sans-serif;
-        margin-top: -.5rem;
-    }
-    input{
-        text-align: center;
-    }
-    
-    .container {
-        width: 100vw;
-        height: 100vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        max-width: 80rem;
-        margin: 0 auto;
-      }
-    
-      button {
-        display: inline-block;
-        font-family: sans-serif;
-        background-color: #5e6df1;
-        border-radius: 10px;
-        border: 3px double #cccccc;
-        color: #eeeeee;
-        text-align: center;
-        font-size: 15px;
-        padding: 20px;
-        width: 200px;
-        margin-top: 3px;
-      }
-    
-      button:hover{
-          box-shadow: 4px 3px 6px #4450bd;
-      }
-    
-    #startpage{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-    
-    #timer{
-        font-family: sans-serif;
-        background-color: #5a69f0;
-        color: #eeeeee;
-        font-size: x-large;
-        text-shadow: .5px .5px black;
-        text-align: center;
-    }
-    #quiz {
-        display: none;
-    }
-    
-    #result {
-        display: none;
-    }
-    
-    #gameover {
-        text-align: center;
-        display: none;
-        flex-direction: column;
-        align-items: center;
-    }
-    
-    #highScore{
-        height: 1.5rem;
-        border: groove 2px;
-    }
-    
-    #high-scorePage{
-        display: none;
-        width: 50%;
-        border: solid;
-        border-color: silver;
-        border-bottom-style: groove;
-        padding-bottom: .3rem;
-        background: lightsteelblue;
-    }
-    
-    #highscore-header{
-        display:flex;
-        justify-content: space-around;
-        border-bottom-style: outset;
-        margin-bottom: .5rem;
-        background-color: #5e6df1;
-        color: #eeeeee;
-        font-family: sans-serif;
-    }
-    #highscoreContainer{
-        height: 100%;
-        width: 100%;
-        display: none;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
-    
-    #initials{
-        margin: .5rem;
-        height: 1.75rem;
-    }
-    
-    #highscore-initials{
-        display: inline-block;
-        padding-left: 9.5rem;
-        font-family: sans-serif;
-        font-weight: bold;
-    }
-    
-    #highscore-score{
-        float: right;
-        padding-right: 8.75rem;
-        font-family: sans-serif;
-        font-weight: bold;
-    }
-    
-    #finalScore{
-        padding-bottom: 0.5rem;
-        font-size: x-large;
-        font-family: sans-serif;
-        font-weight: bold;
-    }
-    
-    #endGameBtns{
-        display: none;
-    }
-    
+}
+
+// This button starts the quiz!
+startQuizButton.addEventListener("click",startQuiz);
